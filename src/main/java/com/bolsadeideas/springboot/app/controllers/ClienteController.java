958ba6994079a4fcaf77a98a7286006b5dcaf974
Nahuel <nahuel_ovejero@outlook.es>
2020-10-21 13:12:10 -0300

    Agregando metodo crear cliente

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bolsadeideas.springboot.app.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.entities.Cliente;
 
 @Controller
 public class ClienteController {
@@ -23,4 +25,19 @@ public class ClienteController {
 		return "listar";
 	}
 
+	@GetMapping("/form")
+	public String crear(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("titulo", "Formulario de cliente");
+		return"form";
+	}
+	
+	
+	@PostMapping("/form")
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
+	}
+	
 }
--- src/main/java/com/bolsadeideas/springboot/app/dao/IClienteDao.java
+++ src/main/java/com/bolsadeideas/springboot/app/dao/IClienteDao.java
@@ -7,4 +7,6 @@ import com.bolsadeideas.springboot.app.entities.Cliente;
 public interface IClienteDao {
 
 	public List<Cliente> findAll();
+	
+	public void save(Cliente cliente);
 }
