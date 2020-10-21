7439d5db4dbeaa2dcfa0c174f88e3d96f815d6fc
Nahuel <nahuel_ovejero@outlook.es>
2020-10-21 11:30:33 -0300

    Agegando estilos con bootstrap a listar.html. Mas datos de prueba a import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1,'Nahuel','Ovejero','nahuel_ovejero@outlook.es','2019-10-10');
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2,'Jonathan','Perez','Jonathan@outlook.es','2015-12-12');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3,'Oscar','Ovejero','oscar_ovejero@outlook.es','2015-10-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (4,'Pedro','Perez','pepe@outlook.es','2012-12-12');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (5,'Agustin','Rodriguez','agus_ro@outlook.com','2017-01-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (6,'Nicolas','Gonzalez','nico@outlook.es','2012-12-10');
